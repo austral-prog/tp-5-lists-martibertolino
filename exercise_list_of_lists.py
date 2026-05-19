6da6dac6eddbb339f8bd9034a3eaf41fd091251a
martibertolino <virginiacesarini195@gmail.com>
2026-05-19 14:23:00 -0300

    Update exercise_list_of_lists.py

--- exercise_list_of_lists.py
+++ exercise_list_of_lists.py
@@ -1,16 +1,5 @@
-# Ejercicio 12: Manipular lista de listas
-
-def list_of_lists(lista_de_listas):
-    """
-    Modifica una lista de 3 listas internas:
-    - Primera lista: solo los primeros 2 elementos
-    - Segunda lista: elementos entre el segundo y cuarto
-    - Tercera lista: solo los últimos 2 elementos
-
-    Args:
-        lista_de_listas: Una lista que contiene 3 listas
-
-    Returns:
-        La lista de listas modificada según las reglas
-    """
-    pass  # Reemplazar con tu implementación
+def list_of_lists (lista):
+    lista[0] = lista[0][:2]
+    lista[1] = lista[1][1:4]
+    lista[2] = lista[2][-2:]
+    return lista
