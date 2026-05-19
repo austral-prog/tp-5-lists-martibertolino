19ff919b83654e3a88a4926f5a58578efca7813d
martibertolino <virginiacesarini195@gmail.com>
2026-05-19 14:23:42 -0300

    Update exercise_remove_elements.py

--- exercise_remove_elements.py
+++ exercise_remove_elements.py
@@ -1,14 +1,2 @@
-# Ejercicio 4: Remover elementos en posiciones específicas
-
-def remove_elements(lista):
-    """
-    Remueve el primer, quinto y sexto elemento de la lista.
-    La función debe funcionar con listas de cualquier tamaño.
-
-    Args:
-        lista: Una lista de elementos
-
-    Returns:
-        La lista después de remover los elementos indicados
-    """
-    pass  # Reemplazar con tu implementación
+def reverse_list(lista):
+    return lista[::-1]
