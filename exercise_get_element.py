7c4f21bf4ba4bee98785bf14acdb649b5064a523
martibertolino <virginiacesarini195@gmail.com>
2026-05-19 14:10:48 -0300

    Update exercise_get_element.py

--- exercise_get_element.py
+++ exercise_get_element.py
@@ -1,7 +1,6 @@
-#def get_element(lista, indice):
+def get_element(lista, indice):
     if indice >= 0 and indice < len(lista):
         return lista[indice]
-
     elif indice < 0 and indice > -len(lista):
         return lista[indice]
     else:
